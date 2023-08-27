5e55d742a10506bd0a40febbcba31f3ad7681a36
Kulpot <kulpotwahu@gmail.com>
2023-08-27 17:11:39 +0800

    Add project files.

---
+++ You Must Unbox to the Exact Type/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=m8H7Ij0_JWo&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=17
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        //int i = 5;
+        //long myLong = i;
+
+        object o = 5;   // boxing
+        //long myLong = (long)o;  // error unbox not exact type
+        long myLong = (int)o;  // exact type unboxing
+    }
+}
